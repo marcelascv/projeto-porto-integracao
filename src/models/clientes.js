fd5eba9d93f2af9c7513dea9e142a28abbbbfe4a
mariliamessias <marilia.messias@mercadolivre.com>
2020-11-08 10:44:05 -0300

    comentário no schema de clientes

--- src/models/clientes.js
+++ src/models/clientes.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const clientesSchema = new mongoose.Schema({
     nome: { type: String },
     email: { type: String },
+    //CPF obrigatório
     cpf: { type: String, required: true },
+    //CPF não obrigatório
+    //cpf: { type: String },
     dataNascimento: { type: String },
     estadoCivil: { type: String },
     telefone: { type: Number },
